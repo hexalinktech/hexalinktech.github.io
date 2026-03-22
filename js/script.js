76a2c17c463f4b06264b6c31682a46edcdc59aed
hexalinktech <hexalink.tech@gmail.com>
2026-03-22 15:11:45 -0600

    Add Intersection Observer for card elements

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ const nav = document.getElementById("nav");
 toggle.addEventListener("click",()=>{
 nav.classList.toggle("active");
 });
+
+const observer = new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+});
+
+const elements = document.querySelectorAll(".card");
+
+elements.forEach(el=>{
+observer.observe(el);
+});
