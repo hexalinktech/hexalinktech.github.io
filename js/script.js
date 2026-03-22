8bff82cc4e155e44cee61d72dd4fce2f7d2ed3aa
hexalinktech <hexalink.tech@gmail.com>
2026-03-22 15:01:05 -0600

    Refactor toggle event listener syntax

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 const toggle = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav");
 
-toggle.addEventListener("click", () => {
-
+toggle.addEventListener("click",()=>{
 nav.classList.toggle("active");
-
 });
